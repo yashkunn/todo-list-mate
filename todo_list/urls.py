1c677ffce75c551f097a69411f1aeaed73084139
Yakiv Lobach <lobachyakiv@gmail.com>
2024-09-25 11:09:02 +0300

    add html to index.html

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
+from todo_list.views import index
+
 app_name = "todo_list"
 
 urlpatterns = [
