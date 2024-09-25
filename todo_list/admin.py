2eb20f70dba8b0e48ace21dfe7705a36f01f986c
Yakiv Lobach <lobachyakiv@gmail.com>
2024-09-25 14:59:30 +0300

    add admin panel

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Tag, Task
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "content", "created_at", "is_done")
+    list_filter = ("is_done", "created_at")
+    search_fields = ("content",)
+    prepopulated_fields = {"content": ("content",)}
