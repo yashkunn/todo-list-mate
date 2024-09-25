6778e7cd6a1d6f130a782f1f6f075b96165d4105
Yakiv Lobach <lobachyakiv@gmail.com>
2024-09-25 11:27:50 +0300

    add class AddTask to views, add url

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from todo_list.views import index
+from todo_list.views import index, AddTaskView
 
 app_name = "todo_list"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("add-task/", AddTaskView.as_view(), name="add-task"),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from todo_list.models import Task
 
@@ -7,3 +9,10 @@ from todo_list.models import Task
 def index(request: HttpRequest) -> HttpResponse:
     tasks = Task.objects.all().order_by("is_done", "-created_at")
     return render(request, "todo_list/index.html", {"tasks": tasks})
+
+
+class AddTaskView(generic.CreateView):
+    model = Task
+    fields = ["content", "deadline"]
+    template_name = "todo_list/add_task.html"
+    success_url = reverse_lazy("index")
